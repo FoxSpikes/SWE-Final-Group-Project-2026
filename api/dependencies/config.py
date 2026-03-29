3cc460c8245ff953f6c91bde44d9e80345c341b9
Robert Williams <rwill262@charlotte.edu>
2026-03-29 19:41:31 -0400

    fix: uvicorn not wanting to work

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,9 @@
+from .password import db_password
 class conf:
     db_host = "localhost"
     db_name = "sandwich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "rootroot"
+    db_password = db_password
     app_host = "localhost"
     app_port = 8000
